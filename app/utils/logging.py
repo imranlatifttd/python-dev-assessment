ae59e1b65316345c0dd1b8654c92cf062cebd006
Imran Latif <imranlatif.ttd@gmail.com>
2026-04-23 00:58:50 +0500

    fix(logging): correct structlog dict_tracebacks attribute

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -8,7 +8,7 @@ def configure_logging() -> None:
             structlog.stdlib.add_log_level,
             structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
-            structlog.processors.DictTracebacks(),
+            structlog.processors.dict_tracebacks,
             structlog.processors.JSONRenderer(),
         ],
         context_class=dict,
